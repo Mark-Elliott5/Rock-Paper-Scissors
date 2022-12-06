5bb19383d53c9544b4a2bf75b7be90b1d80d59e4
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2022-12-06 14:41:28 -0500

    Updated getComputerChoice to not return undefined

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     let weapon = ['Rock', 'Paper', 'Scissors'];
     let computerChoice = '';
-    let indexNumber = Math.floor(Math.random()*3)+1
+    let indexNumber = Math.floor(Math.random()*3)
     return computerChoice = weapon[indexNumber]
 }
