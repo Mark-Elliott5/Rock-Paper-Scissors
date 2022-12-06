36c344dc40cb6f50ed4f261d5ae6e9bf2decb674
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2022-12-06 14:38:23 -0500

    Wrote getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function getComputerChoice() {
+    let weapon = ['Rock', 'Paper', 'Scissors'];
+    let computerChoice = '';
+    let indexNumber = Math.floor(Math.random()*3)+1
+    return computerChoice = weapon[indexNumber]
+}
