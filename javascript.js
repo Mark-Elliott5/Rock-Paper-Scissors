b09775cf85bfb4f070588a93295f38d818b6a59e
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2022-12-06 23:14:44 -0500

    Made smaller functions subdeclarations

--- javascript.js
+++ javascript.js
@@ -1,51 +1,50 @@
-function getComputerChoice() {
-    const weapons = ['rock', 'paper', 'scissors'];
-    let computerChoice = '';
-    let indexNumber = Math.floor(Math.random() * 3);
-    return computerChoice = weapons[indexNumber];
-}
-
-function playRound (playerSelection = prompt(), computerSelection = getComputerChoice()) {
-    const weapons = ['rock', 'paper', 'scissors'];
-    let score = 0;
-    playerSelection = playerSelection.toLowerCase();
-    console.log('The computer chose ' + computerSelection);
-    console.log('The player chose ' + playerSelection);
-    if (playerSelection == computerSelection) {
-        console.log('Draw! Let\'s try again...')
-        return;
-    } else if (playerSelection == 'rock') {
-        if (computerSelection == 'scissors') {
-            console.log('Rock beats Scissors! You win!');
-            return score += 1;
-        }
-        else if (computerSelection == 'paper') {
-            console.log('Paper beats Rock! Get owned son!');
-            return score -= 1;
-        }
-    } else if (playerSelection == 'paper') {
-        if (computerSelection == 'rock') {
-            console.log('Paper beats Rock! You win!');
-            return score += 1;
-        } else if (computerSelection == 'scissors') {
-            console.log('Scissors beats Paper. This dude brought paper to a scissors fight.');
-            return score -= 1;
-        }
-    } else if (playerSelection == 'scissors') {
-        if (computerSelection == 'paper') {
-            console.log('Scissors beats Paper! You win!');
-            return score += 1;
-        } else if (computerSelection == 'rock') {
-            console.log('Rock beats Scissors nerd. Destroyed.');
-            return score -= 1;
-        }
-    } else if (!(weapons.includes(playerSelection))) {
-        console.log('You can\'t use that weapon in Rock Paper Scissors sir. Let\'s try again...');
-        return;
-    }
-}
-
 function game(gameCount = 5) {
+    const weapons = ['rock', 'paper', 'scissors'];
+
+    function getComputerChoice() {
+        let computerChoice = '';
+        let indexNumber = Math.floor(Math.random() * 3);
+        return computerChoice = weapons[indexNumber];
+    }
+
+    function playRound (playerSelection = prompt(), computerSelection = getComputerChoice()) {
+        let score = 0;
+        playerSelection = playerSelection.toLowerCase();
+        console.log('The computer chose ' + computerSelection);
+        console.log('The player chose ' + playerSelection);
+        if (playerSelection == computerSelection) {
+            console.log('Draw! Let\'s try again...')
+            return;
+        } else if (playerSelection == 'rock') {
+            if (computerSelection == 'scissors') {
+                console.log('Rock beats Scissors! You win!');
+                return score += 1;
+            } else if (computerSelection == 'paper') {
+                console.log('Paper beats Rock! Get owned son!');
+                return score -= 1;
+            }
+        } else if (playerSelection == 'paper') {
+            if (computerSelection == 'rock') {
+                console.log('Paper beats Rock! You win!');
+                return score += 1;
+            } else if (computerSelection == 'scissors') {
+                console.log('Scissors beats Paper. This dude brought paper to a scissors fight.');
+                return score -= 1;
+            }
+        } else if (playerSelection == 'scissors') {
+            if (computerSelection == 'paper') {
+                console.log('Scissors beats Paper! You win!');
+                return score += 1;
+            } else if (computerSelection == 'rock') {
+                console.log('Rock beats Scissors nerd. Destroyed.');
+                return score -= 1;
+            }
+        } else if (!(weapons.includes(playerSelection))) {
+            console.log('You can\'t use that weapon in Rock Paper Scissors sir. Let\'s try again...');
+            return;
+        }
+    }
+
     let playerScore = 0;
     let computerScore = 0;
     for (let i = 0; i < (gameCount); i++) {
@@ -60,10 +59,11 @@ function game(gameCount = 5) {
             i--;
         }
     } if (playerScore > computerScore) {
-        console.log(`You won the game! The score was ${playerScore} - ${computerScore}`)
+        console.log(`You won the game! The score was ${playerScore} - ${computerScore}`);
     } else {
-        console.log(`You lost to the computer. The score was ${playerScore} - ${computerScore}`)
+        console.log(`You lost to the computer. The score was ${playerScore} - ${computerScore}`);
     }
 }        
+
 game()
 
