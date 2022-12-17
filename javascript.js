9898ec2d6641313cca884501f3aa9b9c917c51cf
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2022-12-17 14:14:13 -0500

    Game complete, resets score at 5 at next click

--- javascript.js
+++ javascript.js
@@ -1,88 +1,15 @@
-// //This function calls for a game of RPS to be played, the number of rounds
-// //determined by the number that the function is passed, which is default 5
-
-// function game(gameCount = 5) {
-//     const weapons = ['rock', 'paper', 'scissors'];
-
-//     //This is an anonymous function
-//     const getComputerChoice = function () {return weapons[Math.floor(Math.random() * 3)];} 
-
-//     //This function handles individual RPS rounds
-//     function playRound (playerSelection = prompt().toLowerCase(), computerSelection = getComputerChoice()) {
-//         let score = 0;
-//         console.log('The computer chose ' + computerSelection);
-//         console.log('The player chose ' + playerSelection);
-//         if (playerSelection == computerSelection) {
-//             console.log('Draw! Let\'s try again...');
-//             return;
-//         } else if (playerSelection == 'rock') {
-//             if (computerSelection == 'scissors') {
-//                 console.log('Rock beats Scissors! You win!');
-//                 return score += 1;
-//             } else if (computerSelection == 'paper') {
-//                 console.log('Paper beats Rock! Get owned son!');
-//                 return score -= 1;
-//             }
-//         } else if (playerSelection == 'paper') {
-//             if (computerSelection == 'rock') {
-//                 console.log('Paper beats Rock! You win!');
-//                 return score += 1;
-//             } else if (computerSelection == 'scissors') {
-//                 console.log('Scissors beats Paper. This dude brought paper to a scissors fight.');
-//                 return score -= 1;
-//             }
-//         } else if (playerSelection == 'scissors') {
-//             if (computerSelection == 'paper') {
-//                 console.log('Scissors beats Paper! You win!');
-//                 return score += 1;
-//             } else if (computerSelection == 'rock') {
-//                 console.log('Rock beats Scissors nerd. Destroyed.');
-//                 return score -= 1;
-//             }
-//         } else if (!(weapons.includes(playerSelection))) {
-//             console.log('You can\'t use that weapon in Rock Paper Scissors sir. Let\'s try again...');
-//             return;
-//         }
-//     }
-
-//     //This part of the game() function displays W/L at the end of the game and 
-//     //sets number of rounds to be played
-//     let playerScore = 0;
-//     let computerScore = 0;
-//     for (let i = 0; i < (gameCount); i++) {
-//         let result = playRound();
-//         if (result == 1) {
-//             playerScore += 1;
-//             continue;
-//         } else if (result == -1) {
-//             computerScore += 1;
-//             continue;
-//         } else {
-//             i--;
-//         }
-//     } if (playerScore > computerScore) {
-//         console.log(`You won the game! The score was ${playerScore} - ${computerScore}`);
-//     } else {
-//         console.log(`You lost to the computer. The score was ${playerScore} - ${computerScore}`);
-//     }
-// }        
-
-// game()
-
 const weapons = document.querySelectorAll('button');
 weapons.forEach((weapon) => {
-
+    
     weapon.addEventListener('click', playRound);
     
   });
 
-// const playerScore = document.querySelector('playerScore');
-// const computerScore = document.querySelector('computerScore');
-
 function playRound (buttonClicked) {
-    let score = 0;
     const playerScore = document.querySelector('#player-score');
     const computerScore = document.querySelector('#computer-score');
+    const outcomeText = document.querySelector('#outcome-text')
+    const matchText = document.querySelector('#match-text')
     const playerSelection = buttonClicked.currentTarget.id
     const weapons = ['rock', 'paper', 'scissors'];
     //This is an Immediately Invoked Function Expression:
@@ -90,41 +17,40 @@ function playRound (buttonClicked) {
 
     console.log('The computer chose ' + computerSelection);
     console.log('The player chose ' + playerSelection);
-
-    if (playerSelection == computerSelection) {
-        console.log('Draw! Let\'s try again...');
+    if ((playerScore.textContent == 5) || (computerScore.textContent == 5)) {
+        playerScore.textContent = '0';
+        computerScore.textContent = '0';
+        matchText.textContent = '';
+    } if (playerSelection == computerSelection) {
+        outcomeText.textContent = ('Draw! Let\'s try again...');
         return;
-    } else if (playerSelection == 'rock') {
+    } if (playerSelection == 'rock') {
         if (computerSelection == 'scissors') {
-            console.log('Rock beats Scissors! You win!');
-            score += 1;
+            outcomeText.textContent = 'Rock beats Scissors! You win!';
+            playerScore.textContent = ((parseInt(playerScore.textContent)) + 1);
         } else if (computerSelection == 'paper') {
-            console.log('Paper beats Rock! Get owned son!');
-            score -= 1;
+            outcomeText.textContent = 'Paper beats Rock! Get owned son!';
+            computerScore.textContent = ((parseInt(computerScore.textContent)) + 1);
         }
-    } else if (playerSelection == 'paper') {
+    } if (playerSelection == 'paper') {
         if (computerSelection == 'rock') {
-            console.log('Paper beats Rock! You win!');
-            score += 1;
+            outcomeText.textContent = 'Paper beats Rock! You win!';
+            playerScore.textContent = ((parseInt(playerScore.textContent)) + 1);
         } else if (computerSelection == 'scissors') {
-            console.log('Scissors beats Paper. This dude brought paper to a scissors fight.');
-             score -= 1;
+            outcomeText.textContent = 'Scissors beats Paper. This dude brought paper to a scissors fight.';
+            computerScore.textContent = ((parseInt(computerScore.textContent)) + 1);
         }
-    } else if (playerSelection == 'scissors') {
+    } if (playerSelection == 'scissors') {
         if (computerSelection == 'paper') {
-            console.log('Scissors beats Paper! You win!');
-             score += 1;
+            outcomeText.textContent = 'Scissors beats Paper! You win!';
+            playerScore.textContent = ((parseInt(playerScore.textContent)) + 1);
         } else if (computerSelection == 'rock') {
-            console.log('Rock beats Scissors nerd. Destroyed.');
-            score -= 1;
+            outcomeText.textContent = 'Rock beats Scissors nerd. Destroyed.';
+            computerScore.textContent = ((parseInt(computerScore.textContent)) + 1);
         }
-    } else {
-        return console.log('error');
-    }
-
-    if (score > 0) {
-        playerScore.textContent = ((parseInt(playerScore.textContent)) + 1);
-    } if (score < 1) {
-        computerScore.textContent = ((parseInt(computerScore.textContent)) + 1);
-    }
+    } if (playerScore.textContent == 5) {
+        matchText.textContent = 'You won the match! Click a button to play again.';
+    } if (computerScore.textContent == 5) {
+        matchText.textContent = 'You lost the match. Click a button to play again.';
+    }    
 }
